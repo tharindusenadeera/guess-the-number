fe39d6e0c3720497b81ac52c0bfe5e3ad3437ea3
tlsenadeera <tharindu@rydecars.com>
2020-04-18 11:44:17 +0530

    added input component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,37 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 import Card from "../components/Card";
 import Colors from "../constants/colors";
+import Input from "../components/Input";
 
 const StartGameScreen = (props) => {
+  const [enterdValue, setEnteredValue] = useState("");
+
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a new game !</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <TextInput />
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+          onChangeText={numberInputHandler}
+          value={enterdValue}
+        />
         <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent}/></View>
-          <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary}/></View>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+          </View>
         </View>
       </Card>
     </View>
@@ -30,9 +49,9 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   inputContainer: {
-      width: 300,
-      maxWidth: '80%',
-      alignItems: 'center'
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
   },
   buttonContainer: {
     flexDirection: "row",
@@ -41,8 +60,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 120
-  }
+    width: 120,
+  },
+  input: {
+    width: 70,
+    textAlign: "center",
+  },
 });
 
 export default StartGameScreen;
